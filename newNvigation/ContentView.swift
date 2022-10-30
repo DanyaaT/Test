9377902ef6be1ed69f0319f622c3e9b14c5fa897
DanyaaT <Danyaa.tu@gmail.com>
2022-10-31 01:15:20 +0300

    first commit

---
+++ newNvigation/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  newNvigation
+//
+//  Created by Danya T on 05/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            ScrollView{
+        VStack{
+        Text("Hello, world!")
+            .padding()
+        }
+        .navigationTitle("My Team")
+        .toolbar{
+            ToolbarItem(placement:.navigationBarTrailing ){
+            
+                NavigationLink {
+                   Text("hi")
+                } label: {
+                    Image(systemName: "person.crop.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(.blue)
+                }
+            }
+            ToolbarItem(){
+                Image(systemName: "bell.fill").font(.title2)
+                    .foregroundColor(.yellow)
+            }
+            ToolbarItem(placement: .navigationBarLeading){
+                Image(systemName: "person.2.circle").font(.largeTitle)
+                    .foregroundColor(.red)
+            }
+        }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newNvigation/newNvigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  newNvigationApp.swift
+//  newNvigation
+//
+//  Created by Danya T on 05/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct newNvigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
