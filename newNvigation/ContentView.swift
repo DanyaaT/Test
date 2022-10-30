edd414ee89a29ff8949b23c54970295e7e9cbcad
DanyaaT <Danyaa.tu@gmail.com>
2022-10-31 01:40:05 +0300

    c

--- newNvigation/ContentView.swift
+++ newNvigation/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         NavigationView{
             ScrollView{
         VStack{
-        Text("Hello, world!")
+        Text("first push test")
             .padding()
         }
         .navigationTitle("My Team")
